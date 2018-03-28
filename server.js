774edffd20fa0c02209fe2b4dc12994d0f19d384
junyoung-sung <junyoung.sung@navercorp.com>
2018-03-28 17:00:31 +0900

    Add server.js for http server

---
+++ server.js
@@ -0,0 +1,7 @@
+let http = require('http');
+
+console.log("hello world server is running in http://localhost:8080")
+http.createServer(function (req, res) {
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
