989d1b14081d79b1bba4f569052d9425ff506536
junyoung-sung <junyoung.sung@navercorp.com>
2018-04-03 00:25:07 +0900

    Add 4 questions

---
+++ public/js/example.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+    $('.get_result_1').click(function () {
+        let first = $('#first_num_1').val() * 1;
+        let second = $('#second_num_1').val() * 1;
+
+        if (!first || !second) {
+            alert('숫자만 입력해 주세요!!');
+            $('#first_num_1').val('');
+            $('#second_num_1').val('');
+        }
+
+        let result = first + second;
+
+        $('#result_1').val(result * 1000);
+        return 1;
+    });
+
+    $('.get_result_2').click(function () {
+    });
+
+    $('.get_result_3').click(function () {
+    });
+
+    $('.get_result_4').click(function () {
+    });
+});
