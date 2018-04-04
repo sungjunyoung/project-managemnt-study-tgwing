e9b8e57f1bee7dd1633de8a4dde6a775b08d2ffb
junyoung-sung <junyoung.sung@navercorp.com>
2018-04-04 18:03:57 +0900

    Add console.log

--- public/js/example.js
+++ public/js/example.js
@@ -17,17 +17,22 @@ $(document).ready(function () {
     // });
 
     $('.get_result_1').click(function () {
+        console.log('question 1')
     });
 
     $('.get_result_2').click(function () {
+        console.log('question 2')
     });
 
     $('.get_result_3').click(function () {
+        console.log('question 3')
     });
 
     $('.get_result_4').click(function () {
+        console.log('question 4')
     });
 
     $('.get_result_5').click(function () {
+        console.log('question 5')
     });
 });
