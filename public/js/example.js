57509f359ea97a43f5f43d739d70d7fc572e086f
Ryulth <adboy94@khu.ac.kr>
2018-04-04 21:12:54 +0900

    -add question_4 first_result

--- public/js/example.js
+++ public/js/example.js
@@ -30,6 +30,14 @@ $(document).ready(function () {
 
     $('.get_result_4').click(function () {
         console.log('question 4')
+        let first =$('#first_num_4').val() * 1;
+        let value = 1;
+        let first_result =0;
+        do {
+            first_result+=value;
+            value++;
+        }while (value <= first);
+        console.log(first_result);
     });
 
     $('.get_result_5').click(function () {
