1666e30fa463a988e7a23473b26b3af7d3fb2534
DESKTOP-KR1JSK8\user <bsh01111@naver.com>
2018-04-04 21:31:33 +0900

    Sanghyun complete

--- public/js/example.js
+++ public/js/example.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
 
-    // $('.get_result_1').click(function () {
-    //     let first = $('#first_num_1').val() * 1;
-    //     let second = $('#second_num_1').val() * 1;
-    //
-    //     if (!first || !second) {
-    //         alert('숫자만 입력해 주세요!!');
-    //         $('#first_num_1').val('');
-    //         $('#second_num_1').val('');
-    //     }
-    //
-    //     let result = first + second;
-    //
-    //     $('#result_1').val(result * 1000);
-    //     return 1;
-    // });
+    $('.get_result_1').click(function () {
+        let first = $('#first_num_1').val() * 1;
+        let second = $('#second_num_1').val() * 1;
+
+        if (!first || !second) {
+            alert('숫자만 입력해 주세요!!');
+            $('#first_num_1').val('');
+            $('#second_num_1').val('');
+        }
+
+        let result = first + second;
+
+        $('#result_1').val(result * 1000);
+        return 1;
+    });
 
     $('.get_result_1').click(function () {
         console.log('question 1')
@@ -25,7 +25,10 @@ $(document).ready(function () {
     });
 
     $('.get_result_3').click(function () {
-        console.log('question 3')
+      let result = 10
+      $('#result_3').val(result * 1000);
+      return 1;
+
     });
 
     $('.get_result_4').click(function () {
