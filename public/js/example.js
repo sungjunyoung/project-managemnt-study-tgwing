95604e9650ab8f24eefea6621fccd6e3ead7469e
junyoung-sung <junyoung.sung@navercorp.com>
2018-04-04 17:22:01 +0900

    Example source

--- public/js/example.js
+++ public/js/example.js
@@ -1,19 +1,22 @@
 $(document).ready(function () {
 
+    // $('.get_result_1').click(function () {
+    //     let first = $('#first_num_1').val() * 1;
+    //     let second = $('#second_num_1').val() * 1;
+    //
+    //     if (!first || !second) {
+    //         alert('숫자만 입력해 주세요!!');
+    //         $('#first_num_1').val('');
+    //         $('#second_num_1').val('');
+    //     }
+    //
+    //     let result = first + second;
+    //
+    //     $('#result_1').val(result * 1000);
+    //     return 1;
+    // });
+
     $('.get_result_1').click(function () {
-        let first = $('#first_num_1').val() * 1;
-        let second = $('#second_num_1').val() * 1;
-
-        if (!first || !second) {
-            alert('숫자만 입력해 주세요!!');
-            $('#first_num_1').val('');
-            $('#second_num_1').val('');
-        }
-
-        let result = first + second;
-
-        $('#result_1').val(result * 1000);
-        return 1;
     });
 
     $('.get_result_2').click(function () {
@@ -23,5 +26,8 @@ $(document).ready(function () {
     });
 
     $('.get_result_4').click(function () {
+    });
+
+    $('.get_result_5').click(function () {
     });
 });
